a93ae5361bf44e3d04a5ec4d691e54d298258642
uisFer721 <fercast721@gmail.com>
2024-05-04 12:08:20 -0500

    3ra version

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect
+import sqlite3
+
+# Cargamos los datos
+conexion = sqlite3.connect("web2.sqlite3")
+conexion.row_factory = sqlite3.Row # modo diccionario
+cursor = conexion.cursor()
+cursor.execute("""
+SELECT * FROM productos;
+""")
+productos = [dict(producto) for producto in cursor.fetchall()]
+cursor.close()
+conexion.close()
+
+# aplicacion
+
+app = Flask(__name__)
+
+# rustas
+@app.route('/')
+def ruta_raiz():
+    return render_template('index.html', productos=productos)
+
+@app.route('/producto/<int:pid>')
+def ruta_producto(pid):
+   for producto in productos:
+      if pid == producto['id']:
+         return render_template('producto.html', producto=producto)
+   return redirect('/')
+         
+
+# Programa principal
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
